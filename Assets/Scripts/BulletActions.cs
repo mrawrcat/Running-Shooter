953f3abb714fd21fb72059146cfc17de90cd5328
mrawrcat <wdan13@gmail.com>
2021-03-27 21:41:47 -0400

    added action example

---
+++ Assets/Scripts/BulletActions.cs
@@ -0,0 +1,7 @@
+using System;
+using UnityEngine;
+
+public static class BulletActions
+{
+    public static Action<Bullet> OnBulletHit;
+}
---
+++ Assets/Scripts/BulletManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletManager : MonoBehaviour
+{
+    [SerializeField]
+    private int bulletCount;
+
+    [SerializeField]
+    private Text bulletTxt;
+    private void OnEnable()
+    {
+        BulletActions.OnBulletHit += BulletHit;
+    }
+
+    private void OnDisable()
+    {
+        BulletActions.OnBulletHit -= BulletHit;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletTxt.text = bulletCount.ToString();
+    }
+
+    public void BulletHit(Bullet bullet)
+    {
+        bulletCount++;
+    }
+}
